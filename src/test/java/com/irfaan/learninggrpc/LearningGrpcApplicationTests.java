d4fd1cc3a02d458650e748d12eba7d440b805a06
Ahmad Irfaan <irfaan.hibatullah@gmail.com>
2023-04-08 13:21:22 +0700

    add learning grpc init project

---
+++ src/test/java/com/irfaan/learninggrpc/LearningGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irfaan.learninggrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
