c5a4bff40aced237194b50b5b769171209ee6c7c
Ahmad Irfaan <irfaan.hibatullah@gmail.com>
2023-04-08 17:25:01 +0700

    add server grpc start

--- src/main/java/com/irfaan/learninggrpc/LearningGrpcApplication.java
+++ src/main/java/com/irfaan/learninggrpc/LearningGrpcApplication.java
@@ -1,13 +1,22 @@
 package com.irfaan.learninggrpc;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
 
 @SpringBootApplication
 public class LearningGrpcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningGrpcApplication.class, args);
+	public static void main(String[] args) throws IOException, InterruptedException {
+		Server server = ServerBuilder.forPort(50051).build();
+
+		server.start();
+
+		server.awaitTermination();
 	}
 
 }
